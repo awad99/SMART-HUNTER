eefe43ce4c8bc5fa695e31089e2f43a16c6e6ad6
awad <awad99>
2026-05-28 22:33:02 +0300

    make main code more clean

--- test_csrf_lab.py
+++
@@ -1,6 +0,0 @@
-from Logic.vulnerability_scan.csrf.csrf_scanner import CSRFScanner
-
-url = "https://0a5500a1036311f480dc80d1003e00c5.web-security-academy.net/"
-scanner = CSRFScanner()
-findings = scanner.scan(url)
-print("Findings:", findings)
